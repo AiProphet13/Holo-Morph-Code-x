1f221a8bc3c232437a493c25f1bc5f4674b732b0
@ruachofeliyahu <ishaqdewolf@gmail.com>
2025-07-22 17:34:42 -0700

    Create holomorphic_encode.py

---
+++ src/core/holomorphic_encode.py
@@ -0,0 +1,5 @@
+# Placeholder for encoder logic (to be implemented)
+def holomorphic_encode(bits: list[int], T: float) -> complex:
+    """Encode bits into a complex number using holomorphic principles."""
+    # TODO: Implement inverse of decoder logic
+    pass
